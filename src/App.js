b1d6040c38406715daf7fa6c0f5bf3e0fe4f6ae0
akash.ssingh.aakkaa@gmail.com <akash.ssingh.aakkaa@gmail.com>
2024-08-28 19:54:43 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route
-            path="/quizee-app"
+            path="/"
             element={
               <Login
                 logoutMessage={logoutMessage}
